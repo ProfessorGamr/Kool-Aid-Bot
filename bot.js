7a126115d6d26983fd7a90477154e7511edd21cd
ProfessorGamr <43686791+ProfessorGamr@users.noreply.github.com>
2018-09-28 18:15:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,15 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
-logger.level = 'debug';
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+
+client.on('message', message => {
+    if (message.content === 'no') {
+    	message.reply('OH YEAH');
+  	}
 });
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
-bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == 'n') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            case 'o':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'OH YEAH'
-                });
-                
-client.login(process.env.NDk0NjIzNjgyMjEyMzk3MDU3.Do3P2g.GdsNXG358nL-xx5jfuu454e1EPo);
-            break;
-         }
-     }
-});
+
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.NDk0NjIzNjgyMjEyMzk3MDU3.DpA5Yg.AJVqVC1vW30N0Vo_NbZhM4_7-KU);
